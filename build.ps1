4e7707510f97b59dce2d63f0e93f6341fe59944c
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-08-08 18:55:34 -0400

    Fix PS5 version folder compatibility for PS2

--- build.ps1
+++ build.ps1
@@ -3,3 +3,6 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module PSDepend -Force
 Invoke-PSDepend -Force -verbose
+
+# For PS2, after installing with PS5.
+Move-Item C:\temp\pester\*\* -Destination C:\temp\pester -force
